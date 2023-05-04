b3d07213589ffc2b583f71aff28d75a55b78d4aa
Jonathan Cai <jonathancai834@gmail.com>
2023-05-04 16:48:12 -0400

    sync

--- Code/TETRIS/TETRIS.ino
+++ Code/TETRIS/TETRIS.ino
@@ -9,15 +9,7 @@
 
 Tone buzzers[4];
 
-int sevenSegGround[4] = {7,8,9,10};
-
-#define sevenSegData 11
-#define sevenSegClock 13
-#define sevenSegShift 12
-
-// Button Input
-#define inputChannelOne A1
-#define inputChannelTwo A2
+int buttonPins[7] = {13,12,11,10,9,8,7};
 
 // RGB Matrix Ports
 // Power      Port L  42-49
@@ -41,7 +33,7 @@ int powerPins[8] = {42,43,44,45,46,47,48,49}; // Not needed, just for completion
 int RGBpins[3][16] = {
   {21,20,19,18,17,16,15,14,A8,A9,A10,A11,A12,A13,A14,A15},
   {38,39,40,41,50,51,52,53,22,23,24,25,26,27,28,29},
-  {0,1,2,A3,A4,A5,A6,A7,37,36,35,34,33,32,31,30}
+  {2,A1,A2,A3,A4,A5,A6,A7,37,36,35,34,33,32,31,30}
 };
 
 void setup() {
@@ -63,14 +55,6 @@ void setup() {
     }
   }
 
-  for (int i = 0; i < 4; i++) {
-    pinMode(sevenSegGround[i], OUTPUT);
-    digitalWrite(sevenSegGround[i], HIGH);
-  }
-  pinMode(sevenSegData, OUTPUT);  
-  pinMode(sevenSegClock, OUTPUT);
-  pinMode(sevenSegShift, OUTPUT);
-
   // Piezo Buzzers
   pinMode(buzzerOne, OUTPUT);
   pinMode(buzzerTwo, OUTPUT);
@@ -82,11 +66,8 @@ void setup() {
   buzzers[2].begin(buzzerThree);
   buzzers[3].begin(buzzerFour);
 
-  pinMode(inputChannelOne, INPUT);
-  pinMode(inputChannelTwo, INPUT);
-
-  for (int i = 0; i < 4; i++) {
-    digitalWrite(sevenSegGround[i], LOW);
+  for (int i = 0; i < 7; i++) {
+    pinMode(buttonPins[i],INPUT);
   }
 }
 
@@ -117,63 +98,16 @@ void loop() {
   //   }
   // }
 
-
-  writeSevenSeg(8, 0);
-
-
+  delay(100);
   handleInput();
-
 }
 
 void handleInput() {
-  int channelOne = analogRead(inputChannelOne);
-  int channelTwo = analogRead(inputChannelTwo);
-
-  if (channelOne > 850) {
-    Serial.println(channelOne);
-    if (channelOne > 950) {
-
-    } else {
-    }
+  for (int i = 0; i < 7; i++) {
+    Serial.print(digitalRead(buttonPins[i]));
+    Serial.print(" ");
   }
-
-  if (channelTwo > 300) {
-    Serial.println(channelTwo);
-    if (channelTwo > 1000) {
-
-    } else if (channelTwo > 900) {
-
-    } else if (channelTwo > 700) {
-
-    } else if (channelTwo > 600) {
-
-    } else {
-
-    }
-  }
+  Serial.println();
 }
 
-// 0 1 2 3 4 5 6 7 8 9 I O T S Z J L
-const int SevenSegSymbols[17] = {B1111110,B0110000,B1101101,B1111001,B0110011,B1011011,B1011111,B1110000,B1111111,B1111011,B0000110,B0011101,B0000111,B1011011,B1101101,B0001110,B0111000};
-
-void writeSevenSeg(int value, bool includeDecimal) {
-  for(int i = 0; i < 8; i++) {
-    digitalWrite(sevenSegData, LOW);
-    pulsePin(sevenSegClock);
-  }
-  pulsePin(sevenSegShift);
-
-  digitalWrite(sevenSegData, includeDecimal); 
-  pulsePin(sevenSegClock);
-  for(int i = 0; i < 7; i++) {
-    digitalWrite(sevenSegData, SevenSegSymbols[value] & (1<<i));
-    pulsePin(sevenSegClock);
-  }
-  pulsePin(sevenSegShift);
-}
-
-void pulsePin(int pin) {
-  digitalWrite(pin, HIGH);
-  digitalWrite(pin, LOW);
-}
 
