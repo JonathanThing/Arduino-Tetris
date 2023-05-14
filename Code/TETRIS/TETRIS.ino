fd1de654ccc75757958e8fd708b2a3299709b4d4
JonathanThing <89045555+JonathanThing@users.noreply.github.com>
2023-05-14 11:16:05 -0400

    Lets go progress
    I made some prgoress

--- Code/TETRIS/TETRIS.ino
+++ Code/TETRIS/TETRIS.ino
@@ -1,5 +1,14 @@
 #include <Tone.h>
 
+#define MAX_INT 65535
+
+/*
+TODO:
+1. OOP colour class?
+
+*/
+
+
 const byte numberOfBuzzers = 4;
 const byte numberOfButtons = 7;
 
@@ -12,7 +21,7 @@ const byte buttonPins[numberOfButtons] = { 19, 3, 18, 15, 16, 17, 2 };
 bool inputs[numberOfButtons] = { 0, 0, 0, 0, 0, 0, 0 };
 
 // RGB Matrix
-// Power    Shift Register    
+// Power    Shift Register
 // Red 1    Port K (Not inverted)
 // Green 1  Port A (Inverted)
 // Blue 1   Port F (Not Inverted)
@@ -29,33 +38,38 @@ const byte bluePins[8] = { A0, A1, A2, A3, A4, A5, A6, A7 };
 #define storageClockPin 5
 
 struct Colour {
-  byte red;
-  byte green;
-  byte blue;
+  char symbol;
+  byte red;    // 4 bit
+  byte green;  // 4 bit
+  byte blue;   // 4 bit
 };
 
-const Colour RED = { 255, 0, 0 };
-const Colour GREEN = { 255, 0, 0 };
-const Colour BLUE = { 0, 0, 255 };
-const Colour WHITE = { 255, 255, 255 };
-const Colour PURPLE = { 255, 0, 255 };
-const Colour YELLOW = { 255, 128, 0 };
-const Colour ORANGE = { 255, 30, 0 };
-const Colour CYAN = { 0, 255, 255 };
+const Colour RED = { 'R', 16, 0, 0 };
+const Colour GREEN = { 'G', 0, 16, 0 };
+const Colour BLUE = { 'B', 0, 0, 16 };
+const Colour WHITE = { 'W', 16, 16, 16 };
+const Colour PURPLE = { 'P', 16, 0, 16 };
+const Colour YELLOW = { 'Y', 16, 8, 0 };
+const Colour ORANGE = { 'O', 16, 4, 0 };
+const Colour CYAN = { 'C', 0, 16, 16 };
+
+Colour colours[8] = { RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE };
 
 byte currentRow = 0;
 
 //Col, Row
-Colour display[8][16];
+Colour display[8][16] = {{ RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE, RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE }, { RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE, RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE }, { RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE, RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE }, { RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE, RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE }, { RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE, RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE }, { RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE, RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE }, { RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE, RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE }, { RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE, RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE }};
 
 const int timePerFrame = 15000;
 const int timePerColour = timePerFrame / 3;
 const int timePerRow = timePerColour / 8;
+const int timePerResolution = (timePerRow - 17 * 16) / 16;
 
 long anchorTime = 0;
 long deltaTime = 0;
 
 void setup() {
+  randomSeed(32131);
   Serial.begin(9600);
   // Buttons
   for (int i = 0; i < numberOfButtons; i++) {
@@ -88,18 +102,117 @@ void setup() {
 
   digitalWrite(storageClockPin, LOW);
   digitalWrite(shiftClockPin, LOW);
-
   writeShiftRegister(0);
+
+  
+
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 16; j++) {
+      int thing = random(0,8);
+      Serial.print(thing);
+      Serial.print(" ");
+      display[i][j] = colours[thing];
+    }
+    Serial.println();
+  }
 
   // anchorTime = millis();
 }
 
+int redTime = 0;
+int blueTime = 0;
+int greenTime = 0;
+
 void loop() {
+
   // deltaTime = millis() - anchorTime;
 
-  cyclePower();
-  handleInput();
-  delay(50);
+  for (int i = 0; i < 8; i++) {
+    // Red
+    displayRed();
+    displayGreen();
+    displayBlue();
+
+    // writeRed(0);
+    // delayMicroseconds(redTime);
+    // writeRed(MAX_INT);
+    // delayMicroseconds(timePerRow - redTime);
+
+    // // Green
+    // writeGreen(0);
+    // delayMicroseconds(greenTime);
+    // writeGreen(MAX_INT);
+    // delayMicroseconds(timePerRow - greenTime);
+
+    // // Blue
+    // writeBlue(0);
+    // delayMicroseconds(blueTime);
+    // writeBlue(MAX_INT);
+    // delayMicroseconds(timePerRow - blueTime);
+
+    cyclePower();
+  }
+}
+
+void displayRed() {
+  for (int i = 0; i < 16; i++) {  //Resolution
+    int bitMask = 0;
+    for (int j = 0; j < 16; j++) {
+      bitMask |= (display[currentRow][j].red >= (i + 1));
+      if (j != 15) { 
+       bitMask <<= 1;
+      }
+    }
+    writeRed(~bitMask);
+    delayMicroseconds(timePerResolution);
+  }
+  writeRed(MAX_INT);
+}
+
+void displayGreen() {
+  for (int i = 0; i < 16; i++) {  //Resolution
+    int bitMask = 0;
+    for (int j = 0; j < 16; j++) {
+      bitMask |= (display[currentRow][j].green >= (i + 1));
+      if (j != 15) { 
+       bitMask <<= 1;
+      }
+    }
+    writeGreen(~bitMask);
+    delayMicroseconds(timePerResolution);
+  }
+  writeGreen(MAX_INT);
+}
+
+void displayBlue() {
+  for (int i = 0; i < 16; i++) {  //Resolution
+    int bitMask = 0;
+    for (int j = 0; j < 16; j++) {
+      bitMask |= (display[currentRow][j].blue >= (i + 1));
+      if (j != 15) { 
+       bitMask <<= 1;
+      }
+    }
+    writeBlue(~bitMask);
+    delayMicroseconds(timePerResolution);
+  }
+  writeBlue(MAX_INT);
+}
+
+// LEFT MSB 1000000010000000 LSB RIGHT
+void writeRed(int value) {
+  PORTL = (byte)(value >> 8 & 255);
+  PORTK = reverseByte((byte)value);  // PORT K IS NOT INVERTED
+}
+
+void writeGreen(int value) {
+  PORTC = (byte)(value >> 8 & 255);
+  PORTA = (byte)(value);
+}
+
+void writeBlue(int value) {
+  PORTB = (byte)(value >> 8 & 255);
+  PORTF = reverseByte((byte)value);  // PORT F IS NOT INVERTED
 }
 
 void cyclePower() {
@@ -118,14 +231,24 @@ void cyclePower() {
   currentRow++;
   if (currentRow >= 8) {
     currentRow = 0;
-  } 
+  }
+}
+
+byte reverseByte(byte input) {
+  byte output = input;
+  for (int i = 0; i < 8; i++) {
+    output <<= 1;
+    output |= input & 1;
+    input >>= 1;
+  }
+  return output;
 }
 
 void writeShiftRegister(byte value) {
   digitalWrite(storageClockPin, LOW);
   digitalWrite(shiftClockPin, LOW);
   for (int i = 7; i >= 0; i--) {
-    digitalWrite(dataPin, (value & (1<<i)));
+    digitalWrite(dataPin, (value & (1 << i)));
     digitalWrite(shiftClockPin, HIGH);
     digitalWrite(shiftClockPin, LOW);
   }
