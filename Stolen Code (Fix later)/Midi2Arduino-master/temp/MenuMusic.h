96676bf8183b6c3f509d4b20dec42913a8278a4f
JonathanThing <89045555+JonathanThing@users.noreply.github.com>
2023-06-18 23:56:51 -0400

    Removed borrowed code

--- Stolen Code (Fix later)/Midi2Arduino-master/temp/MenuMusic.h
+++
@@ -1,6 +0,0 @@
-#ifndef MENU_MUSIC_H
-#define MENU_MUSIC_H
-#include "MusicTrack.h"
-#include "Arduino.h"
-extern MusicTrack tracksMenuMusic[];
-#endif
--- Stolen Code (Fix later)/Midi2Arduino-master/temp/TetrisMusic.h
+++
@@ -1,6 +0,0 @@
-#ifndef TETRIS_MUSIC_H
-#define TETRIS_MUSIC_H
-#include "MusicTrack.h"
-#include "Arduino.h"
-extern MusicTrack tracksTetrisMusic[];
-#endif
